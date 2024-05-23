2421e654cbddd0b1fde001e6e4c7b2a7ccedeec5
MiaMohn <mfatimagaiop@gmail.com>
2024-05-23 14:28:28 +0200

    Insert delete and update components for expense

--- src/app/features/expense/expense-list/expense-list.component.ts
+++ src/app/features/expense/expense-list/expense-list.component.ts
@@ -6,11 +6,13 @@ import { ExpenseService } from '../../../services/expense/expense.service';
 import { User } from '../../../models/user.model';
 import { UserService } from '../../../services/user/user.service';
 import { AddExpenseComponent } from '../add-expense/add-expense.component';
+import { DeleteExpenseComponent } from '../delete-expense/delete-expense.component';
+import { UpdateExpenseComponent } from '../update-expense/update-expense.component';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, AddExpenseComponent],
+  imports: [CommonModule, RouterModule, AddExpenseComponent, DeleteExpenseComponent, UpdateExpenseComponent],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css',
 })
