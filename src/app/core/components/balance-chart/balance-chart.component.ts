f0a54a01823bbb4c242cbd9e8320312326d50493
MiaMohn <mfatimagaiop@gmail.com>
2024-05-27 10:21:28 +0200

    Create new component balance-chart

---
+++ src/app/core/components/balance-chart/balance-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './balance-chart.component.html',
+  styleUrl: './balance-chart.component.css'
+})
+export class BalanceChartComponent {
+
+}
